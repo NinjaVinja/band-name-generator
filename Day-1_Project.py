2e8a7d1d1efae34ecabf5d3b164842410261124b
NinjaVinja <taha.ahmad4700@gmail.com>
2025-07-18 21:50:58 +0500

    Add files via upload

---
+++ Day-1_Project.py
@@ -0,0 +1,11 @@
+# 🎸 Displaying a welcome message for the user
+print("🎸 Welcome to the Band Name Generator 🎤")
+
+# 📍 Asking the user for the name of the city they grew up in
+city_name = input("What's the name of the city you grew up in? \n")
+
+# 🐶 Asking the user for their pet's name
+pets_name = input("What's your pet's name? \n")
+
+# 🎤 Combining the city name and pet name to create a band name
+print("Your band name could be: " + city_name + " " + pets_name)
